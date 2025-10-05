94c509628bff08525a086c69d0bd46b269076e24
IsmailAmine97 <76774370+IsmailAmine97@users.noreply.github.com>
2025-10-05 17:47:06 +0200

    Ismail

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -36,7 +36,7 @@ public class PersonService {
 		p = new Person();
 		p.setId("4");
 		p.setAge(28);
-		p.setFirstName("IsmailCasablancaVersailles");
+		p.setFirstName("Ismail");
 		p.setLastName("Amine");
 		persons.add(p);
 		
